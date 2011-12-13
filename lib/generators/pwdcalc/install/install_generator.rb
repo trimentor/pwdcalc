a70141b9e24a8de27e955d3c27bf901f25fddf86
Kjel Delaey <kjel_delaey@hotmail.com>
2011-12-13 21:47:13 +0100

    Create install generator: copy files (assets and locales)

---
+++ lib/generators/pwdcalc/install/install_generator.rb
@@ -0,0 +1,18 @@
+module Pwdcalc
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
+
+      if ::Rails.version[0..2].to_f >= 3.1
+        # Rails 3.1 has the asset pipeline, no need to copy JS files any more.
+      else
+        copy_file "../../../../../app/assets/javascripts/jquery.YAPSM.min.js", "public/javascripts/jquery.YAPSM.min.js"
+      end
+
+      def copy_locales
+        copy_file "pwdcalc.en.yml", "config/locales/pwdcalc.en.yml"
+      end
+
+    end
+  end
+end
