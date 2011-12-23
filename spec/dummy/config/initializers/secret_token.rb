1c4bf2c2aed857f9f7ce0e386d042017cac3c196
Kjel Delaey <kjel_delaey@hotmail.com>
2011-12-23 16:09:21 +0100

    Set-up RSpec with a dummy application

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '65ee057a5a96dc6ef2e1914c5110e3d40efc9c5b053b948d06e7b2b7e18cf45216e5b62fcff79a5620b4b99676816f05ed54e27f1032cb87616a67c141833295'
