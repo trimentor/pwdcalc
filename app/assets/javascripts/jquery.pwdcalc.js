c917bd9eff8583f7c04ea699d6594905f632d88b
Kjel Delaey <kjel_delaey@hotmail.com>
2012-04-30 12:39:50 +0200

    trigger 'pwdcalc-submit' event on form submit. This may be useful to perform additional validation (e.g. minimum length)

--- app/assets/javascripts/jquery.pwdcalc.js
+++ app/assets/javascripts/jquery.pwdcalc.js
@@ -5,31 +5,36 @@
  * https://github.com/trimentor/pwdcalc/blob/master/LICENSE
  */
 $(document).ready(function () {
-    $(".pwdcalc").each(function () {
-        var passwordStrengthMeter, passwordField, passwordScore;
+    $("li.pwdcalc").each(function () {
+        var pwdcalc, passwordStrengthMeter, passwordField, passwordScore;
 
-        passwordStrengthMeter = this;
-        passwordField = "#" + $(this).data('field');
-        passwordScore = $(passwordField).next(".pwdcalc-score");
+        pwdcalc = $(this);
+        passwordStrengthMeter = $('.pwdcalc', pwdcalc);
+        passwordField = $('#'+passwordStrengthMeter.data('field'));
+        passwordScore = passwordField.next(".pwdcalc-score");
 
-        $(passwordField).yapsm().keyup(function () {
+        $(this).closest('form').submit(function(event) {
+            $(document).trigger('pwdcalc-submit', [event, passwordField]);
+        });
+
+        passwordField.yapsm().keyup(function () {
             var strength, localizedHint;
 
             strength = this.complexity;
             localizedHint = $(".pwdcalc-hint[data-complexity='" + strength + "']").html();
 
             if (!$.trim(this.value).length) {
-                $(passwordStrengthMeter).hide();
-                $(passwordStrengthMeter).empty();
-                $(passwordScore).hide();
+                passwordStrengthMeter.hide();
+                passwordStrengthMeter.empty();
+                passwordScore.hide();
             } else {
-                $(passwordStrengthMeter).show();
-                $(passwordStrengthMeter).attr('data-complexity', strength);
-                $(passwordScore).attr('data-strength', strength).show();
+                passwordStrengthMeter.show();
+                passwordStrengthMeter.attr('data-complexity', strength);
+                passwordScore.attr('data-strength', strength).show();
                 if (!$.trim(localizedHint).length) {
-                    $(passwordStrengthMeter).html(strength);
+                    passwordStrengthMeter.html(strength);
                 } else {
-                    $(passwordStrengthMeter).html(localizedHint);
+                    passwordStrengthMeter.html(localizedHint);
                 }
             }
         });
