f96e8bc79223d28751f386d5b3a460c8596d5d77
Kjel Delaey <kjel_delaey@hotmail.com>
2013-06-25 21:25:35 +0200

    config.action_view.javascript_expansions is no longer used since Rails 4

--- lib/pwdcalc.rb
+++ lib/pwdcalc.rb
@@ -5,7 +5,9 @@ module Pwdcalc
   # Required for jquery.YAPSM.min.js and jquery.pwdcalc.js to be discoverable in the asset pipeline
   class Engine < ::Rails::Engine
     config.before_initialize do
-      config.action_view.javascript_expansions[:pwdcalc] = %w(jquery.YAPSM.min.js jquery.pwdcalc.js)
+      if config.action_view.javascript_expansions
+        config.action_view.javascript_expansions[:pwdcalc] = %w(jquery.YAPSM.min.js jquery.pwdcalc.js)
+      end
     end
 
     initializer "pwdcalc.initialize" do
